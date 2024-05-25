5bfb1262b48ea1fdffdd4816a663ceaaaae25140
jaffamonkey <paullittlebury@gmail.com>
2024-05-25 21:24:36 +0100

    updates

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -2,8 +2,8 @@ module.exports = {
 	name: "Sample", // optional, falls back to object key
 	description: "The default sites that get tested",
 	options: {
-		runs: 1,
-		frequency: 1, // (in minutes)
+		runs: 10,
+		frequency: 30, // (in minutes)
 	},
 	urls: [
 		"https://jaffamonkey.com/",
